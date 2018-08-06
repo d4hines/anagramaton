f43ee6e98b344ce5c78890507f682bccb976737b
Daniel Hines <d4hines@gmail.com>
2018-08-06 07:57:41 -0400

    finished

--- src/anagramizer/core.clj
+++ src/anagramizer/core.clj
@@ -1,49 +1,49 @@
 (ns anagramizer.core
-  (:require [clojure.set :as set]
-            [clojure.string :as str]
+  (:require [clojure.string :as str]
             [clojure.math.combinatorics :as combo]
             [trie.core :refer [trie]]
             [multiset.core :as ms]))
 
-(defn partial-anagram1? [word subword]
-  (->> [word subword]
+(defn clean-string
+  "Lowercases and removes whitespace and apostrophes from a string.
+
+  KNOWN ISSUE: This get's carried away sometimes. For example, it replaces \"Buñuel\" with buuel."
+  [x]
+  (-> x
+      str/lower-case
+      (str/replace #"\W|'" "")))
+
+(defn anagram-key
+  "Sorts a string alphabetically. The value of the anagram key is the same for all other anagrams, e.g (anagram-key \"read\") and (anagram-key \"dare\") are equal."
+  [x]
+
+  (-> x vec sort str/join))
+
+(defn dict->word-map
+  "Takes a dictionary of words in the form of a collection of strings and returns a map of anagram keys and the words associated with that key.
+
+  Example usage: (dict->word-map (str/split (slurp \"/usr/share/dict/words\") #\"\\n\"))"
+  [dict]
+  (->> dict
+       set
+       (map clean-string)
+       (reduce (fn [agg curr]
+                 (update agg
+                         (anagram-key curr)
+                         #(into #{curr} %))) `{})))
+
+(defn partial-anagram?
+  "Returns true if word contains at least all the letter in subword."
+  [word subword]
+  (->> [subword word]
        (map #(apply ms/multiset (seq %)))
        (apply ms/subset?)))
 
-(defn partial-anagram? [word subword]
-  (let [f-hist (frequencies word)
-        sub-hist (frequencies subword)]
-    (and (set/subset? (set (keys sub-hist)) (set (keys f-hist)))
-         (every? second (merge-with >= f-hist sub-hist)))))
-
-(partial-anagram? "hello" "hello")
-
-(defn clean-string [x] (-> x
-                           str/lower-case
-                           (str/replace #"\W|'" "")))
-
-(defn anagram-key [x]
-  (-> x vec sort str/join))
-
-(def wordset (as-> "/usr/share/dict/words" x
-               (slurp x)
-               (str/split x #"\n")
-               ;; For Dev only, remember to delete!
-               ;; (take 1000 x)
-               (map clean-string x)
-               (set x)))
-
-;; Map of all single word anagrams in the word set
-(def word-map (reduce (fn [agg curr]
-                        (update agg
-             (anagram-key curr)
-                                #(into #{curr} %)))
-                      {} wordset))
-
-(def anagram-set (set (map anagram-key wordset)))
-
 (defn partial-anagrams
-  [word]
+  "Returns all the words that can be formed from any combination of the letters in word.
+
+  Note: not used by the rest of library, but useful for playing with anagrams in general."
+  [word-map word]
   (reduce (fn [matches [ana-key ana-words]]
             (if (partial-anagram? word ana-key)
               (into matches ana-words)
@@ -51,9 +51,12 @@
           #{} word-map))
 
 (defn partial-anagram-keys
-  [word]
+  "Returns all the partial anagrams of word in sorted key form."
+  [word-map word]
   (reduce (fn [matches [ana-key ana-words]]
-            ;; only allow a, i, and o as one-leter words
+            ;; The unix dictionary has an entry for every letter of the alphabet.
+            ;; This makes things messy, so as a workaround we filter out any
+            ;; one-letter words besides a, i, and o.
             (if (and (or (> (count ana-key) 1)
                          (contains? #{"a" "i" "o"} ana-key))
                      (partial-anagram? word ana-key))
@@ -61,38 +64,10 @@
               matches))
           #{} word-map))
 
-(defn partial-anagram-keys-multiset
-  [x]
-  (->> x
-       partial-anagram-keys
-       (map (partial apply ms/multiset))
-       set))
-
-(def adore-key (anagram-key "adore"))
-
 (defn str->ms [s] (apply ms/multiset (seq s)))
 
-(def ak-ms (str->ms adore-key))
-
-(def paks (partial-anagram-keys adore-key))
-
-(def paks-ms (partial-anagram-keys-multiset "adore"))
-
-(def deor (apply ms/multiset (seq "deor")))
-(def aer (apply ms/multiset (seq "aer")))
-(def adoe (apply ms/multiset (seq "adoe")))
-(def a (apply ms/multiset (seq "a")))
-
-(map #(ms/minus ak-ms %) paks-ms)
-
-;; not a solution
-(contains? paks-ms (ms/minus ak-ms adoe))
-
-;; a solution
-(contains? paks-ms (ms/minus ak-ms aer))
-(map str/join [(ms/minus ak-ms aer) aer])
-
 (defn ms-subtract
+  "Returns the multiset difference between a and b (or any number of multisets) if b is a subset of a, otherwise returns ::not-subset."
   ([a b]
    (if (ms/subset? b a)
      (ms/minus a b)
@@ -105,97 +80,38 @@
 (defn str-ms-subtract [mset s]
   (ms-subtract mset (str->ms s)))
 
-(def t (trie paks))
-
 (def empty-ms (ms/multiset))
 
 (defn anagram-helper
-  [current-match t mset solution]
-  (let [ms-diff
-        (->> current-match
-             ;; convert it to a multiset
-             str->ms
-             ;; return the difference between the word
-             (ms-subtract mset))
-        working-solution (conj solution current-match)]
-    [ms-diff working-solution]))
+  "Recursively searches through the trie for words, using the multiset difference to narrow the search.Returns every combination of anagram keys that can "
+  [t mset partial-solution solutions]
+  (if (= mset empty-ms) (conj solutions partial-solution)
+      (let [letter (str (first mset))
+            matches (t letter)
+            output
+            (map (fn [current-match]
+                   (let [sol (conj partial-solution current-match)
+                         nms (str-ms-subtract
+                              mset current-match)]
+                     (if (not= nms ::not-subset)
+                       (anagram-helper t nms sol solutions)))) matches)]
+        (->> output
+             (filter identity)
+             (mapcat identity)))))
 
-(anagram-helper "a" t (str->ms "adeor") [])
-
-(defn pass-helper2
-  [t new-ms partial-solution solutions]
-  (cond (= new-ms empty-ms) (conj solutions partial-solution)
-        :else (let [letter (str (first new-ms))
-                    matches (t letter)
-                    output
-                    (map (fn [current-match]
-                           (let [[nms sol]
-                                 (anagram-helper
-                                  current-match t new-ms partial-solution)]
-                             (if (not= nms ::not-subset)
-                               (pass-helper2 t nms sol solutions)
-                               ))) matches)]
-                (println "passhelper" output)
-                (->> output
-                    (filter identity)
-                    (mapcat identity)))))
-
-(contains? anagram-set "ar")
-
-
-(pass-helper2 t ak-ms [] [])
-
-(count(pass-helper2 t ak-ms [] []))
-
-(pass-helper2 t (str->ms "deo") ["ar"] [])
-
-(defn helper2 [word mset]
-  (let [options (t word)]
-    (zipmap
-     (map vector options)
-     (map (partial str-ms-subtract mset) options))))
-
-{["a"] #{\d \e \o \r},
- ["aor"] #{\d \e},
- ["ar"] #{\d \e \o},
- ["aer"] #{\d \o},
- ["ad"] #{\e \o \r},
- ["ader"] #{\o},
- ["adeor"] #{},
- ["ado"] #{\e \r},
- ["ador"] #{\e}}
-
-(let [[possible-sol msdif] (-> (helper2 "a" ak-ms) vec (nth 2))]
-  [possible-sol msdif])
-
-(def pass1 (anagram-helper "a" t ak-ms []))
-
-(def pass2 (let [[prev-word new-ms solution next-letter] pass1]
-             (anagram-helper next-letter t new-ms solution)))
-
-(def pass3 (let [[prev-word new-ms solution next-letter] pass2]
-             (anagram-helper next-letter t new-ms solution)))
-
-(anagram-helper "a" t ak-ms [])
-
-(ms-subtract ak-ms (str->ms "z"))
-
-(defn anagram2
-  [letters]
-  (let [ak (anagram-key letters)
+(defn anagrams
+  "Returns every anagram word or phrase that can be formed from the given letters as a sequence of word sequences."
+  [word-map letters]
+  (let [ak (anagram-key (clean-string letters))
         ak-ms (str->ms ak)
-        paks (partial-anagram-keys ak)
-        paks-ms (partial-anagram-keys-multiset ak)
-          ;; t is a trie structure of every partial anagram key
-          ;; You can ask it for every paks that begins with a given string
-        t (trie paks)]
-    (->> ak first str
-         t
-         first
-         str->ms
-         (ms-subtract ak-ms))))
-
-(anagram2 "adore")
-
-((anagram2 "adore") "a")
+        paks (partial-anagram-keys word-map ak)
+        ;; A trie structure of every partial anagram the letters can form.
+        t (trie paks)
+        akeys (anagram-helper t ak-ms [] [])]
+    ;; anagram-helper returns combination of anagram keys, not words.
+    ;; Therefore, it's necessary to map the keys back to the corresponding words.
+    (set (mapcat (fn [x] (->> x
+                              (map #(get word-map %))
+                              (apply combo/cartesian-product)))
+                 akeys))))
 
--- test/anagramizer/core_test.clj
+++ test/anagramizer/core_test.clj
@@ -1,7 +1,72 @@
 (ns anagramizer.core-test
   (:require [clojure.test :refer :all]
-            [anagramizer.core :refer :all]))
+            [anagramizer.core :as a]
+            [trie.core :refer [trie]]
+            [multiset.core :as ms]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest clean-string
+  (is (= (a/clean-string "A' s") "as")))
+
+(deftest clean-string
+  (is (= (a/anagram-key "adore") "adeor"))
+  (is (= (a/anagram-key "zyyyx") "xyyyz")))
+
+(deftest dict->word-map
+  (let [dict ["read" "dare" "boo"]
+        expected {"ader" #{"read" "dare"} "boo" #{"boo"}}
+        result (a/dict->word-map dict)]
+    (is (=  result expected))))
+
+(deftest partial-anagram?
+  (is (a/partial-anagram? "foobar" "oob"))
+  (is (a/partial-anagram? "foobar" "rfa"))
+  (is (not (a/partial-anagram? "foo" "foob"))))
+
+(deftest partial-anagram-keys
+  (testing "Should only return a, i, and o as single letter words"
+    (let [dict ["a" "b" "c" "i" "o"]
+          word-map (a/dict->word-map dict)]
+      (is (= (a/partial-anagram-keys word-map "abcio")
+             #{"a" "i" "o"}))))
+
+  (testing "Besides single-letter words, should return all partial anagrams"
+    (let [dict ["adore" "dare" "read" "red"]
+          word-map (a/dict->word-map dict)]
+      (is (= (a/partial-anagram-keys word-map "adore")
+             #{"adeor" "ader" "der"})))))
+
+(def ms ms/multiset)
+
+(deftest ms-subtract
+  (testing "Should subtract any number of multisets from minuend"
+    (is (= (a/ms-subtract (ms :a :b :c) (ms :b) (ms :c))
+           (ms :a))))
+  (testing "Should return ::not-subet if the sum of the subtrehends is not a subset of the minuend"
+    (is (= (a/ms-subtract (ms :a :b :c) (ms :a :a))
+           ::a/not-subset))))
+
+(def word-map (a/dict->word-map ["adore" "dare" "read" "o"]))
+(def paks (a/partial-anagram-keys word-map "adore"))
+(def t (trie paks))
+
+(deftest anagram-helper
+  (testing "On base case where the input multiset is empty, should add the partial solution to the solutions list"
+    (is (= (a/anagram-helper (trie) a/empty-ms ["foo"] [])
+           [["foo"]])))
+  (testing "Should return every anagram key for a given multiset and partial-anagram key set trie"
+    (let [word-map word-map
+          paks paks
+          t t]
+      (is (= (a/anagram-helper t (a/str->ms "adore") [] [])
+             `(["ader" "o"] ["adeor"]))))))
+
+(deftest anagrams
+  (testing "Should return no anagrams for impossible anagrams"
+    (let [word-map word-map]
+      (is (= (a/anagrams word-map "qwer")
+             #{}))))
+  (testing "Should return every anagram of a given string"
+    (let [word-map word-map]
+      (is (= (set (a/anagrams word-map "adore"))
+             #{`("dare" "o") `("read" "o") `("adore")})))))
+
